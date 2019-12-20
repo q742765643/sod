0b74c8d2ef682d23c730e3a06d58939cf1a193a6
weiliguo <EDZ@WINDOWS-2C8B9RG>
2019-12-20 16:32:31 +0800

    mod

--- sod/sod-system/sod-system-rpc-service/src/main/java/com/piesat/sod/system/rpc/service/DbFileServiceImpl.java
+++ sod/sod-system/sod-system-rpc-service/src/main/java/com/piesat/sod/system/rpc/service/DbFileServiceImpl.java
@@ -17,11 +17,11 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import com.alibaba.druid.util.StringUtils;
 import com.piesat.common.jpa.BaseDao;
 import com.piesat.common.jpa.BaseService;
-import com.piesat.common.jpa.page.PageBean;
-import com.piesat.common.jpa.page.PageForm;
 import com.piesat.sod.system.dao.DbFileDao;
 import com.piesat.sod.system.entity.DbFileEntity;
 import com.piesat.sod.system.rpc.api.DbFileService;
+import com.piesat.util.page.PageBean;
+import com.piesat.util.page.PageForm;
 
 
 /** 数据库文件 业务处理
@@ -51,36 +51,39 @@ public class DbFileServiceImpl extends BaseService<DbFileEntity> implements DbFi
 	 * @param pageForm
 	 * @return
 	 */
+	@SuppressWarnings("rawtypes")
 	@Override
 	public PageBean findPageData(Map<String, Object> params, PageForm pageForm) throws Exception{
+		if (null == pageForm) pageForm = new PageForm();
+		StringBuffer sb = new StringBuffer();
 	
-		String sql = " SELECT "
+		sb.append( " SELECT "
 				+ " record_id,file_type,file_name,file_stor_name,file_stor_path, "
 				+ " update_time,file_picture,file_suffix "
 				+ " FROM dmin_db_file "
-				+ " where 1=1 ";
+				+ " where 1=1 ");
 		if(null != params.get("fileType") && !StringUtils.isEmpty(params.get("fileType")+"")) {
-			sql += " and file_type = '"+params.get("fileType")+"' ";
+			sb.append( " and file_type = '"+params.get("fileType")+"' ");
 		}
 		if(null != params.get("fileName") && !StringUtils.isEmpty(params.get("fileName")+"")) {
-			sql += " and file_name like '%"+params.get("fileName")+"%' ";
+			sb.append(" and file_name like '%"+params.get("fileName")+"%' ");
 		}
 		if(null != params.get("fileSuffix") && !StringUtils.isEmpty(params.get("fileSuffix")+"")) {
-			sql += " and file_suffix like '%"+params.get("fileSuffix")+"%' ";
+			sb.append( " and file_suffix like '%"+params.get("fileSuffix")+"%' ");
 		}
 		if(null != params.get("fileStartDate") && !StringUtils.isEmpty(params.get("fileStartDate")+"")) {
-			sql += " and  update_time >= '"+params.get("fileStartDate")+" 00:00:00' ";
+			sb.append( " and  update_time >= '"+params.get("fileStartDate")+" 00:00:00' ");
 		}
 		if(null != params.get("fileEndDate") && !StringUtils.isEmpty(params.get("fileEndDate")+"")) {
-			sql += " and  update_time <= '"+params.get("fileEndDate")+" 59:59:59' ";
+			sb.append(" and  update_time <= '"+params.get("fileEndDate")+" 59:59:59' ");
 		}
 		if(null != params.get("field") && !StringUtils.isEmpty(params.get("field")+"")
 				&& null != params.get("order") && !StringUtils.isEmpty(params.get("order")+"")) {
-			sql += " order by "+params.get("field") + params.get("order");
+			sb.append( " order by "+params.get("field") + params.get("order"));
 		}else {
-			sql += "order by update_time desc";
+			sb.append( "order by update_time desc");
 		}
-		PageBean page = this.queryByNativeSQLPageList(sql, DbFileEntity.class, null, pageForm);
+		PageBean page = this.queryByNativeSQLPageList(sb.toString(), DbFileEntity.class, null, pageForm);
 		return page;
 	}
 
