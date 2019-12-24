b27cc7a18186f8f5d59788e4b7e665bb7a01eb71
yululuya <whaleyl@163.com>
2019-12-24 21:08:36 +0800

    数据库访问账户

---
+++ sod-ui/src/views/authorityAudit/DBaccount/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="DB-container">
+   数据库访问账户
+  </div>
+</template>
+
+<script>
+import { listUser, getUser, delUser, addUser, updateUser, exportUser, resetUserPwd, changeUserStatus } from "@/api/system/user";
+import { treeselect } from "@/api/system/dept";
+import { listPost } from "@/api/system/post";
+import { optionselect } from "@/api/system/role";
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+
+export default {
+  components: { Treeselect },
+  data() {
+    return {
+     
+    };
+  },
+  watch: {
+    
+  },
+  created() {
+    
+  },
+  methods: {
+   
+  }
+};
+</script>
