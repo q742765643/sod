5beefe31f8cda0c422b96128d902925a154505c7
cuiwenhui <810073399@qq.com>
2020-01-17 11:14:24 +0800

    添加数据库管理基础类

--- sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/TableColumnServiceImpl.java
+++ sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/TableColumnServiceImpl.java
@@ -9,6 +9,7 @@ import com.piesat.dm.rpc.dto.TableColumnDto;
 import com.piesat.dm.rpc.mapper.TableColumnMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -62,6 +63,7 @@ public class TableColumnServiceImpl extends BaseService<TableColumnEntity> imple
     }
 
     @Override
+    @Transactional(readOnly = false)
     public int deleteByIdIn(List<String> ids) {
         return this.tableColumnDao.deleteByIdIn(ids);
     }
