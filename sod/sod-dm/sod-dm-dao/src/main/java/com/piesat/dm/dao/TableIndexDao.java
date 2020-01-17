10bd0e6daac379c82fef9409af531e21344dc1b3
wangyaya0708 <1078590298@qq.com>
2020-01-17 15:24:11 +0800

    添加接口：根据表id查列信息

--- sod/sod-dm/sod-dm-dao/src/main/java/com/piesat/dm/dao/TableIndexDao.java
+++ sod/sod-dm/sod-dm-dao/src/main/java/com/piesat/dm/dao/TableIndexDao.java
@@ -4,6 +4,9 @@ import com.piesat.common.jpa.BaseDao;
 import com.piesat.dm.entity.TableIndexEntity;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TableIndexDao extends BaseDao<TableIndexEntity> {
+    List<TableIndexEntity> findByTableId(String tableId);
 }
