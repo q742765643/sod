c6225145f1ecec389bd54e3705fab49a29551e10
wangyaya0708 <1078590298@qq.com>
2020-02-05 10:17:41 +0800

    同步配置

---
+++ sod/sod-schedule-center/sod-schedule-dao/src/main/java/com/piesat/schedule/entity/sync/SyncMappingEntity.java
@@ -0,0 +1,53 @@
+package com.piesat.schedule.entity.sync;
+
+import com.piesat.common.jpa.entity.BaseEntity;
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * @author yaya
+ * @description TODO
+ * @date 2020/1/17 16:37
+ */
+@Data
+@Entity
+@Table(name = "T_SOD_JOB_SYNCMAPPING_INFO")
+@DiscriminatorValue("SYNCMAPPING")
+public class SyncMappingEntity {
+
+    @Id
+    @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 对应filter里的record_id
+     */
+    @Column(name="source_table_id", length = 50)
+    private String sourceTableId;
+
+    /**
+     * 源数据表名
+     */
+    @Column(name="source_table_name", length = 50)
+    private String sourceTableName;
+
+    /**
+     * 对应config里的record_id
+     */
+    @Column(name="target_table_id", length = 50)
+    private String targetTableId;
+
+    /**
+     * 目标表名
+     */
+    @Column(name="target_table_name", length = 50)
+    private String targetTableName;
+
+    /**
+     * 列映射关系
+     */
+    @Column(name="mapping", columnDefinition="TEXT",nullable=true)
+    private String mapping;
+}
---
+++ sod/sod-schedule-center/sod-schedule-dao/src/main/java/com/piesat/schedule/entity/sync/SyncTaskLogEntity.java
@@ -0,0 +1,93 @@
+package com.piesat.schedule.entity.sync;
+
+import com.piesat.common.jpa.entity.BaseEntity;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * @author yaya
+ * @description TODO
+ * @date 2020/1/15 14:16
+ */
+@Data
+@Entity
+@Table(name = "T_SOD_JOB_SYNCTASK_INFO_LOG")
+@DiscriminatorValue("SYNCTASKLOG")
+public class SyncTaskLogEntity extends BaseEntity {
+    /**
+     * 任务id
+     */
+    @Column(name="task_id", length = 32)
+    private String taskId;
+
+    /**
+     * 日志级别
+     */
+    @Column(name="log_level")
+    private Integer logLevel;
+
+    /**
+     * 日志时间
+     */
+    @Column(name="log_time")
+    private Date logTime;
+
+    /**
+     * 任务状态
+     */
+    @Column(name="state")
+    private Integer state;
+
+    /**
+     * 处理总记录数
+     */
+    @Column(name="counts")
+    private Integer counts;
+
+    /**
+     * 插入记录数
+     */
+    @Column(name="insert_count")
+    private Integer insertCount;
+
+    /**
+     * 更新记录数
+     */
+    @Column(name="update_count")
+    private Integer updateCount;
+
+    /**
+     * 丢弃记录数
+     */
+    @Column(name="discard_count")
+    private Integer discardCount;
+
+    /**
+     * 开始时间
+     */
+    @Column(name="begin_time")
+    private Date beginTime;
+
+    /**
+     * 结束时间
+     */
+    @Column(name="end_time")
+    private Date endTime;
+
+    /**
+     * 耗时
+     */
+    @Column(name="spend_time")
+    private Long spendTime;
+
+    /**
+     * 错误信息
+     */
+    @Column(name="error", length = 1000)
+    private String error;
+}
