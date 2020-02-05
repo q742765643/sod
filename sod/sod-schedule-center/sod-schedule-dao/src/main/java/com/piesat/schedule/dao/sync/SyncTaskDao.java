e42597cf9126d3b544d69000511e9a9c4b7b9add
wangyaya0708 <1078590298@qq.com>
2020-02-05 10:17:06 +0800

    同步配置

---
+++ sod/sod-schedule-center/sod-schedule-dao/src/main/java/com/piesat/schedule/dao/sync/SyncTaskDao.java
@@ -0,0 +1,14 @@
+package com.piesat.schedule.dao.sync;
+
+import com.piesat.common.jpa.BaseDao;
+import com.piesat.schedule.entity.sync.SyncTaskEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author yaya
+ * @description TODO
+ * @date 2020/1/13 16:52
+ */
+@Repository
+public interface SyncTaskDao extends BaseDao<SyncTaskEntity> {
+}
---
+++ sod/sod-schedule-center/sod-schedule-dao/src/main/java/com/piesat/schedule/dao/sync/SyncTaskLogDao.java
@@ -0,0 +1,14 @@
+package com.piesat.schedule.dao.sync;
+
+import com.piesat.common.jpa.BaseDao;
+import com.piesat.schedule.entity.sync.SyncTaskLogEntity;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author yaya
+ * @description TODO
+ * @date 2020/1/15 14:15
+ */
+@Repository
+public interface SyncTaskLogDao extends BaseDao<SyncTaskLogEntity> {
+}
