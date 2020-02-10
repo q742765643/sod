97deb85765c8626b8459f09ed68047a38911e44c
namewulei <namewulei@163.com>
2020-02-10 22:22:26 +0800

    表结构导出

---
+++ sod/sod-dm/sod-dm-rpc-api/src/main/java/com/piesat/dm/rpc/api/TableExportService.java
@@ -0,0 +1,37 @@
+package com.piesat.dm.rpc.api;
+
+import com.piesat.common.grpc.annotation.GrpcHthtService;
+import com.piesat.common.grpc.constant.SerializeType;
+import com.piesat.dm.rpc.dto.TableIndexDto;
+import com.piesat.util.constant.GrpcConstant;
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 表结构导出
+ *
+ * @author wulei
+ * @date 2019年 11月22日 15:38:16
+ */
+@GrpcHthtService(server = GrpcConstant.DM_SERVER, serialization = SerializeType.PROTOSTUFF)
+public interface TableExportService {
+    /**
+     * 获取导出的数据结构
+     * @return
+     */
+    Map<String, Object> getExportMap(String physicalDBParam, String dataClassIdParam);
+
+    /**
+     * 查询逻辑库名称
+     * @return
+     */
+    String getLogicName(String database_logic);
+
+    /**
+     * 获取物理库名称
+     * @return
+     */
+    String getDatabaseName(String physical_id);
+}
