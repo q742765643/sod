7204c6942a904b37d47d73eb4a7a70e2827ed934
cuiwenhui <810073399@qq.com>
2020-02-10 16:56:30 +0800

    添加数据库管理基础类

--- sod/sod-dm/sod-dm-web/src/main/java/com/piesat/dm/web/controller/GridAreaController.java
+++ sod/sod-dm/sod-dm-web/src/main/java/com/piesat/dm/web/controller/GridAreaController.java
@@ -1,8 +1,17 @@
 package com.piesat.dm.web.controller;
 
+import com.piesat.dm.rpc.api.GridAreaService;
+import com.piesat.dm.rpc.dto.GridAreaDto;
+import com.piesat.sso.client.annotation.Log;
+import com.piesat.sso.client.enums.BusinessType;
+import com.piesat.util.ResultT;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 区域信息
@@ -14,5 +23,60 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dm/gridarea")
 @RestController
 public class GridAreaController {
+    @Autowired
+    private GridAreaService gridAreaService;
 
+    @ApiOperation(value = "新增")
+    @RequiresPermissions("dm:gridarea:add")
+    @Log(title = "区域信息管理", businessType = BusinessType.INSERT)
+    @PostMapping(value = "/save")
+    public ResultT save(@RequestBody GridAreaDto gridAreaDto) {
+        try {
+            GridAreaDto save = this.gridAreaService.saveDto(gridAreaDto);
+            return ResultT.success(save);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failed(e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "根据id查询")
+    @RequiresPermissions("dm:gridarea:get")
+    @GetMapping(value = "/get")
+    public ResultT get(String id) {
+        try {
+            GridAreaDto gridAreaDto = this.gridAreaService.getDotById(id);
+            return ResultT.success(gridAreaDto);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failed(e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "根据id删除")
+    @RequiresPermissions("dm:gridarea:del")
+    @Log(title = "区域信息管理", businessType = BusinessType.DELETE)
+    @DeleteMapping(value = "/del")
+    public ResultT del(String id) {
+        try {
+            this.gridAreaService.delete(id);
+            return ResultT.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failed(e.getMessage());
+        }
+    }
+
+    @ApiOperation(value = "查询所有")
+    @RequiresPermissions("dm:gridarea:all")
+    @GetMapping(value = "/all")
+    public ResultT all() {
+        try {
+            List<GridAreaDto> all = this.gridAreaService.all();
+            return ResultT.success(all);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultT.failed(e.getMessage());
+        }
+    }
 }
