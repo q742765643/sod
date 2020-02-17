1cfbcd96dd12972bf3a31e7e7ede9ced72f8c539
weiliguo <EDZ@WINDOWS-2C8B9RG>
2020-02-17 18:17:56 +0800

    add:新增公共元数据同步任务配置增删改查接口,同步记录查询和删除接口

---
+++ sod/sod-schedule-center/sod-schedule-dao/src/main/java/com/piesat/schedule/dao/mmd/ComMetadataSyncCfgDao.java
@@ -0,0 +1,17 @@
+package com.piesat.schedule.dao.mmd;
+
+import org.springframework.stereotype.Repository;
+
+import com.piesat.common.jpa.BaseDao;
+import com.piesat.schedule.entity.mmd.ComMetadataSyncCfgEntity;
+
+/** 同步任务配置
+*@description
+*@author wlg
+*@date 2020年2月6日下午4:42:10
+*
+*/
+@Repository
+public interface ComMetadataSyncCfgDao extends BaseDao<ComMetadataSyncCfgEntity>{
+
+}
