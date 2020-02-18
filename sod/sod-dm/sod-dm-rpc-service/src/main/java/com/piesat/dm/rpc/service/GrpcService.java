781c4eb18dda683df2d1eedd4d03c126a65a02af
cuiwenhui <810073399@qq.com>
2020-02-18 18:04:20 +0800

    调整结构

--- sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/GrpcService.java
+++ sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/GrpcService.java
@@ -1,19 +1,36 @@
 package com.piesat.dm.rpc.service;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.piesat.common.grpc.annotation.GrpcHthtClient;
 import com.piesat.common.utils.StringUtils;
+import com.piesat.dm.dao.DatabaseDao;
+import com.piesat.dm.dao.DatabaseDefineDao;
 import com.piesat.dm.dao.StorageConfigurationDao;
+import com.piesat.dm.entity.DatabaseDefineEntity;
 import com.piesat.dm.entity.StorageConfigurationEntity;
 import com.piesat.dm.mapper.MybatisQueryMapper;
 import com.piesat.dm.rpc.api.DataClassService;
+import com.piesat.dm.rpc.api.DataLogicService;
+import com.piesat.dm.rpc.api.LogicDefineService;
+import com.piesat.dm.rpc.dto.LogicDatabaseDto;
+import com.piesat.dm.rpc.dto.LogicDefineDto;
+import com.piesat.dm.rpc.dto.LogicStorageTypesDto;
 import com.piesat.dm.rpc.mapper.StorageConfigurationMapper;
 import com.piesat.schedule.rpc.api.backup.BackupService;
 import com.piesat.schedule.rpc.api.clear.ClearService;
 import com.piesat.schedule.rpc.api.sync.SyncTaskService;
 import com.piesat.schedule.rpc.dto.sync.SyncTaskDto;
+import com.piesat.ucenter.rpc.api.system.DictDataService;
+import com.piesat.ucenter.rpc.dto.system.DictDataDto;
 import com.piesat.util.ResultT;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author yaya
@@ -30,6 +47,15 @@ public class GrpcService {
     private MybatisQueryMapper mybatisQueryMapper;
     @Autowired
     private DataClassService dataClassService;
+    @Autowired
+    private LogicDefineService logicDefineService;
+    @Autowired
+    private DatabaseDefineDao databaseDefineDao;
+    @Autowired
+    private DataLogicService dataLogicService;
+
+    @GrpcHthtClient
+    private DictDataService dictDataService;
     @GrpcHthtClient
     private ClearService clearService;
     @GrpcHthtClient
@@ -40,13 +66,13 @@ public class GrpcService {
 
     public ResultT updateColumnValue(String id, String column, String value) {
         //删除配置
-        if("3".equals(value)){
+        if ("3".equals(value)) {
             //获取策略配置
             StorageConfigurationEntity storage = this.storageConfigurationDao.findById(id).get();
             if (column.equals("storage_define_identifier")) {
                 //删除存储结构
                 dataClassService.deleteByDataClassId(storage.getDataClassId());
-            }else if(column.equals("sync_identifier")){
+            } else if (column.equals("sync_identifier")) {
                 //删除同步配置
                 String task_id = storage.getSyncId();
                 if (StringUtils.isNotNullString(task_id)) {
@@ -55,13 +81,13 @@ public class GrpcService {
                         syncTaskService.deleteSync(task_id);
                     }
                 }
-            }else if(column.equals("moveclean_identifier")){
+            } else if (column.equals("moveclean_identifier")) {
                 //删除迁移清楚
                 String taskId = storage.getClearId();
                 if (StringUtils.isNotNullString(taskId)) {
                     clearService.deleteClearByIds(new String[]{taskId});
                 }
-            }else if(column.equals("backup_identifier")){
+            } else if (column.equals("backup_identifier")) {
                 //删除备份
                 String taskId = storage.getBackupId();
                 if (StringUtils.isNotNullString(taskId)) {
@@ -70,9 +96,9 @@ public class GrpcService {
             }
         }
         //更新状态
-        mybatisQueryMapper.updateStorageConfigurationStatus(id,column,value);
+        mybatisQueryMapper.updateStorageConfigurationStatus(id, column, value);
 
-        return  ResultT.success();
+        return ResultT.success();
     }
 
     public ResultT deleteById(String id) {
@@ -97,5 +123,71 @@ public class GrpcService {
         //删除配置信息
         storageConfigurationDao.deleteById(id);
         return ResultT.success();
+    }
+
+    public List<Map<String, Object>> getLogicInfo() {
+        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
+
+        // 获取逻辑库类型
+        List<LogicDefineDto> logiclist = logicDefineService.all();
+        //查询物理库-逻辑库
+        List<Map<String, Object>> distinctDatabaseAndLogic = dataLogicService.getDistinctDatabaseAndLogic();
+        //查询所有的表类型
+        Map<String, String> dictMap = new HashMap<String, String>();
+        List<DictDataDto> dictDataDtos = dictDataService.selectDictDataByType("table_type");
+        if (dictDataDtos != null) {
+            for (DictDataDto dictDataDto : dictDataDtos) {
+                dictMap.put(dictDataDto.getDictValue(), dictDataDto.getDictLabel());//E_table 要素表
+            }
+        }
+
+        if (logiclist != null && logiclist.size() > 0) {
+            for (int i = logiclist.size() - 1; i >= 0; i--) {
+                LogicDefineDto logicDefineDto = logiclist.get(i);
+                Map<String, Object> logicDefineMap = JSONObject.parseObject(JSON.toJSONString(logicDefineDto));
+                //逻辑库对应的表类型
+                List<LogicStorageTypesDto> logicStorageTypesEntityList = logicDefineDto.getLogicStorageTypesEntityList();
+                List<Map<String, Object>> tableTypeList = new ArrayList<Map<String, Object>>();
+                if (logicStorageTypesEntityList != null) {
+                    for (LogicStorageTypesDto logicStorageTypesDto : logicStorageTypesEntityList) {
+                        Map<String, Object> map = new HashMap<String, Object>();
+                        String storageType = logicStorageTypesDto.getStorageType();
+                        map.put("key", storageType);
+                        map.put("name", dictMap.get(storageType));
+                        tableTypeList.add(map);
+                    }
+                }
+                logicDefineMap.put("tableType", tableTypeList);
+
+                //逻辑库对应的物理库
+                List<Map<String, Object>> databaseList = new ArrayList<Map<String, Object>>();
+                for (Map<String, Object> databaseLogic : distinctDatabaseAndLogic) {
+                    if (logicDefineDto.getLogicFlag().toString().equals(String.valueOf(databaseLogic.get("logic_flag")))) {
+                        databaseList.add(databaseLogic);
+                    }
+                }
+                if (databaseList.size() != 0) {
+                    logicDefineMap.put("physics", databaseList);
+                    result.add(logicDefineMap);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public List<LogicDefineDto> getAllLogicDefine() {
+        List<LogicDefineDto> all = this.logicDefineService.all();
+        for (LogicDefineDto ld : all) {
+            List<LogicDatabaseDto> logicDatabaseEntityList = ld.getLogicDatabaseEntityList();
+            for (LogicDatabaseDto ldb : logicDatabaseEntityList) {
+                ldb.setDatabaseName(this.databaseDefineDao.findById(ldb.getDatabaseDefineId()).get().getDatabaseName());
+            }
+            List<LogicStorageTypesDto> logicStorageTypesEntityList = ld.getLogicStorageTypesEntityList();
+            for (LogicStorageTypesDto ls:logicStorageTypesEntityList) {
+                ls.setName(this.dictDataService.findByDictTypeAndDictValue("sys_storage_type",ls.getStorageType()).getDictLabel());
+            }
+        }
+        return all;
     }
 }
