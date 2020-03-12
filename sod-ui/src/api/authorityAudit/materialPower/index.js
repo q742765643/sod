fb9366561777d7dd4fd5dad18d2e4efaedbc6011
yululuya <whaleyl@163.com>
2020-03-12 08:54:40 +0800

    资料访问权限审核

--- sod-ui/src/api/authorityAudit/materialPower/index.js
+++ sod-ui/src/api/authorityAudit/materialPower/index.js
@@ -1,10 +1,39 @@
 import request from '@/utils/request'
 const baseUrl = process.env.VUE_APP_DM
 
+// 查询列表
 export function queryList(query) {
   return request({
     url: baseUrl + '/dm/dataAuthorityApply/list',
     method: 'get',
     params: query
+  })
+}
+
+
+// 根据申请id查询申请信息
+export function getApplyInfoById(query) {
+  return request({
+    url: baseUrl + '/dm/dataAuthorityApply/getApplyInfoById',
+    method: 'get',
+    params: query
+  })
+}
+
+// 根据申请id查询申请资料信息
+export function getRecordByApplyId(query) {
+  return request({
+    url: baseUrl + '/dm/dataAuthorityApply/getRecordByApplyId',
+    method: 'get',
+    params: query
+  })
+}
+
+// 审核拒绝
+export function updateRecordCheck(data) {
+  return request({
+    url: baseUrl + '/dm/dataAuthorityApply/updateRecordCheck',
+    method: 'PUT',
+    params: data
   })
 }
