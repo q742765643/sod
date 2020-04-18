b6f641f161b1486803efc004a23e3c6ac1732376
wangyaya0708 <1078590298@qq.com>
2020-04-18 11:04:42 +0800

    功能完善

---
+++ sod/sod-dm/sod-dm-dao/src/main/java/com/piesat/dm/dao/dataapply/NewdataTableColumnDao.java
@@ -0,0 +1,17 @@
+package com.piesat.dm.dao.dataapply;
+
+import com.piesat.common.jpa.BaseDao;
+import com.piesat.dm.entity.dataapply.NewdataTableColumnEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * @author yaya
+ * @description TODO
+ * @date 2020/4/18 9:52
+ */
+@Repository
+public interface NewdataTableColumnDao extends BaseDao<NewdataTableColumnEntity> {
+    List<NewdataTableColumnEntity> findByApplyId(String applyId);
+}
