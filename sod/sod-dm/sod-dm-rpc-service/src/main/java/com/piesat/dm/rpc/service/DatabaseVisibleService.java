4a1173050a946ca5b5ac9ea38495f50ac8be4be1
cuiwenhui <810073399@qq.com>
2020-05-04 22:31:42 +0800

    优化

--- sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/DatabaseVisibleService.java
+++ sod/sod-dm/sod-dm-rpc-service/src/main/java/com/piesat/dm/rpc/service/DatabaseVisibleService.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.piesat.common.grpc.annotation.GrpcHthtClient;
 import com.piesat.common.utils.AESUtil;
+import com.piesat.common.utils.StringUtils;
 import com.piesat.dm.dao.database.DatabaseDao;
 import com.piesat.dm.dao.dataclass.DataLogicDao;
 import com.piesat.dm.dao.datatable.DataTableDao;
@@ -184,7 +185,7 @@ public class DatabaseVisibleService {
                     List<DataTableDto> byClassLogicId = dataTableService.getByClassLogicId(dl.getId());
                     for (DataTableDto dt : byClassLogicId) {
                         JSONObject jo = new JSONObject();
-                        jo.put("TABLE_NAME", dt.getTableName());
+                        jo.put("TABLE_NAME", dotById.getSchemaName() + "." + dt.getTableName());
                         jo.put("DATA_SERVICE_NAME", dt.getDataServiceName());
                         jo.put("D_DATA_ID", byDataClassId.getDDataId());
                         jo.put("STORAGE_TYPE", dl.getStorageType());
@@ -206,6 +207,11 @@ public class DatabaseVisibleService {
     }
 
     public ResultT getTableInfo(String tableName) {
+        String schemaName = "";
+        if (tableName.indexOf(".") != -1) {
+            schemaName = tableName.substring(0, tableName.lastIndexOf("."));
+            tableName = tableName.substring(tableName.lastIndexOf(".") + 1);
+        }
         JSONObject jo = new JSONObject();
         List<DataTableEntity> byTableName = dataTableDao.findByTableName(tableName);
         if (byTableName != null) {
@@ -216,7 +222,11 @@ public class DatabaseVisibleService {
             table_info.put("D_DATA_ID", dataClassDto.getDDataId());
             table_info.put("DATA_CLASS_ID", dataClassId);
             table_info.put("DATA_SERVICE_ID", dataTableEntity.getDataServiceId());
-            table_info.put("TABLE_NAME", dataTableEntity.getTableName());
+            if (StringUtils.isEmpty(schemaName)) {
+                table_info.put("TABLE_NAME", dataTableEntity.getTableName());
+            } else {
+                table_info.put("TABLE_NAME", schemaName + "." + dataTableEntity.getTableName());
+            }
             table_info.put("DATA_SERVICE_NAME", dataTableEntity.getDataServiceName());
             jo.put("table_info", table_info);
             JSONArray table_structure = new JSONArray();
