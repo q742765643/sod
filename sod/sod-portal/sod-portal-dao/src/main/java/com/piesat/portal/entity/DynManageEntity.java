e715e1c71716b067db42fcd48993771e2336a984
wangyaya0708 <1078590298@qq.com>
2020-09-04 15:15:17 +0800

    业务动态管理（portal）

---
+++ sod/sod-portal/sod-portal-dao/src/main/java/com/piesat/portal/entity/DynManageEntity.java
@@ -0,0 +1,44 @@
+package com.piesat.portal.entity;
+
+import com.piesat.common.jpa.entity.BaseEntity;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * 业务动态管理
+ */
+@Data
+@Table(name = "T_SOD_PORTAL_DYN_MANAGE")
+@Entity
+public class DynManageEntity extends BaseEntity {
+
+    /**
+     * 标题
+     */
+    @Column(name="TITLE")
+    private String title;
+
+    /**
+     * 正文
+     */
+    @Column(name="CONTENT",columnDefinition="TEXT",nullable=true)
+    private String content;
+
+
+    /**
+     * 动态类型
+     */
+    @Column(name="NEWS_TYPE")
+    private String newsType;
+
+
+    /**
+     * 发布状态
+     */
+    @Column(name="ISPUBLISHED")
+    private String ispublished;
+
+}
---
+++ sod/sod-portal/sod-portal-rpc-api/src/main/java/com/piesat/portal/rpc/api/DynManageService.java
@@ -0,0 +1,25 @@
+package com.piesat.portal.rpc.api;
+
+import com.piesat.common.grpc.annotation.GrpcHthtService;
+import com.piesat.common.grpc.constant.SerializeType;
+import com.piesat.portal.rpc.dto.DynManageDto;
+import com.piesat.util.constant.GrpcConstant;
+import com.piesat.util.page.PageBean;
+import com.piesat.util.page.PageForm;
+
+import java.util.List;
+
+/**
+ * 业务动态管理
+ */
+@GrpcHthtService(server = GrpcConstant.PORTAL_SERVER, serialization = SerializeType.PROTOSTUFF)
+public interface DynManageService {
+
+    PageBean selectPageList(PageForm<DynManageDto> pageForm);
+
+    DynManageDto saveDto(DynManageDto dynManageDto);
+
+    DynManageDto updateDto(DynManageDto dynManageDto);
+
+    public void deleteRecordByIds(List<String> ids);
+}
