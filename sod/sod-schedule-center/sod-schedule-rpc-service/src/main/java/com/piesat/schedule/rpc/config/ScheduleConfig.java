562eb5f84dc89fe758247d466a4451765b309aa0
zengzhaojin <742765643@qq.com>
2020-09-07 10:28:38 +0800

    修改任务初始化

--- sod/sod-schedule-center/sod-schedule-rpc-service/src/main/java/com/piesat/schedule/rpc/config/ScheduleConfig.java
+++ sod/sod-schedule-center/sod-schedule-rpc-service/src/main/java/com/piesat/schedule/rpc/config/ScheduleConfig.java
@@ -15,11 +15,13 @@ import com.piesat.schedule.rpc.api.move.MoveService;
 import com.piesat.schedule.rpc.dto.backup.BackUpDto;
 import com.piesat.schedule.rpc.dto.clear.ClearDto;
 import com.piesat.schedule.rpc.dto.move.MoveDto;
+import com.piesat.schedule.rpc.lock.RedisLock;
 import com.piesat.schedule.rpc.mapstruct.backup.BackupMapstruct;
 import com.piesat.schedule.rpc.mapstruct.clear.ClearMapstruct;
 import com.piesat.schedule.rpc.mapstruct.move.MoveMapstruct;
 import com.piesat.schedule.rpc.thread.ScheduleThread;
 import com.piesat.schedule.rpc.thread.SendThread;
+import com.piesat.sso.client.util.RedisUtil;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +29,8 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -64,7 +68,10 @@ public class ScheduleConfig implements ApplicationRunner {
     private MoveDao moveDao;
     @Autowired
     private ClearDao clearDao;
-
+    @Autowired
+    private RedisUtil redisUtil;
+    @Autowired
+    private RedisLock redisLock;
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
 
@@ -72,41 +79,57 @@ public class ScheduleConfig implements ApplicationRunner {
         scheduleThread.start();
         ThreadFactory timingPoolFactory = new ThreadFactoryBuilder().setNameFormat("worker-timing-pool-%d").build();
         timingPool = Executors.newScheduledThreadPool(1, timingPoolFactory);
+        //long nowTime=System.currentTimeMillis();
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND,0);
+        long nextTime =calendar.getTime().getTime();
+        System.out.println("下次执行时间:"+nextTime);
+
         timingPool.scheduleWithFixedDelay (()->{
-            try {
-                List<BackupEntity> backupEntityList = backupDao.findAll();
-                List<BackUpDto> backUpDtoList = backupMapstruct.toDto(backupEntityList);
-                for (BackUpDto backUpDto : backUpDtoList) {
-                    try {
-                        backupService.updateBackup(backUpDto);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+            boolean flag=redisLock.lock("init_job");
+            if(flag){
+                try {
+                    List<BackupEntity> backupEntityList = backupDao.findAll();
+                    List<BackUpDto> backUpDtoList = backupMapstruct.toDto(backupEntityList);
+                    for (BackUpDto backUpDto : backUpDtoList) {
+                        try {
+                            backupService.updateBackup(backUpDto);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
 
-                List<MoveEntity> moveEntityList = moveDao.findAll();
-                List<MoveDto> moveDtoList = moveMapstruct.toDto(moveEntityList);
-                for (MoveDto moveDto : moveDtoList) {
-                    try {
-                        moveService.updateMove(moveDto);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    List<MoveEntity> moveEntityList = moveDao.findAll();
+                    List<MoveDto> moveDtoList = moveMapstruct.toDto(moveEntityList);
+                    for (MoveDto moveDto : moveDtoList) {
+                        try {
+                            moveService.updateMove(moveDto);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
-                }
 
-                List<ClearEntity> clearEntityList = clearDao.findAll();
-                List<ClearDto> clearDtoList = clearMapstruct.toDto(clearEntityList);
-                for (ClearDto clearDto : clearDtoList) {
-                    try {
-                        clearService.updateClear(clearDto);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    List<ClearEntity> clearEntityList = clearDao.findAll();
+                    List<ClearDto> clearDtoList = clearMapstruct.toDto(clearEntityList);
+                    for (ClearDto clearDto : clearDtoList) {
+                        try {
+                            clearService.updateClear(clearDto);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally {
+                    redisLock.delete("init_job");
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        },0, 1, TimeUnit.DAYS);
+
+        },nextTime-System.currentTimeMillis(), 86400000, TimeUnit.MILLISECONDS);
     }
 }
 
