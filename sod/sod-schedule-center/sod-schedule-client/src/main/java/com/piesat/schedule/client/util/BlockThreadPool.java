af163dcf642fd59ae60f7c2bc1bf373202f9028b
zengzhaojin <742765643@qq.com>
2020-09-20 15:22:49 +0800

    修改

---
+++ sod/sod-schedule-center/sod-schedule-client/src/main/java/com/piesat/schedule/client/util/BlockThreadPool.java
@@ -0,0 +1,11 @@
+package com.piesat.schedule.client.util;
+
+/**
+ * @ClassName : BlockThreadPool
+ * @Description :
+ * @Author : zzj
+ * @Date: 2020-09-20 15:20
+ */
+public class BlockThreadPool {
+}
+
