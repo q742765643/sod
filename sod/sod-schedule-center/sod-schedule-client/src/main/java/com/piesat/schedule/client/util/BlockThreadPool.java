534a520fa5e3702b14477e551a11d73cd7e3b56d
zengzhaojin <742765643@qq.com>
2020-09-20 15:23:15 +0800

    修改

--- sod/sod-schedule-center/sod-schedule-client/src/main/java/com/piesat/schedule/client/util/BlockThreadPool.java
+++ sod/sod-schedule-center/sod-schedule-client/src/main/java/com/piesat/schedule/client/util/BlockThreadPool.java
@@ -1,5 +1,8 @@
 package com.piesat.schedule.client.util;
 
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @ClassName : BlockThreadPool
  * @Description :
@@ -7,5 +10,45 @@ package com.piesat.schedule.client.util;
  * @Date: 2020-09-20 15:20
  */
 public class BlockThreadPool {
+    private ThreadPoolExecutor pool = null;
+
+    public BlockThreadPool(int poolSize) {
+        pool = new ThreadPoolExecutor(poolSize, poolSize, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(5), new CustomThreadFactory(),
+                new CustomRejectedExecutionHandler());
+    }
+
+    public void destory() {
+        if (pool != null) {
+            pool.shutdownNow();
+        }
+    }
+
+    private class CustomThreadFactory implements ThreadFactory {
+        private AtomicInteger count = new AtomicInteger(0);
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            String threadName = BlockThreadPool.class.getSimpleName() + count.addAndGet(1);
+            t.setName(threadName);
+            return t;
+        }
+    }
+
+    private class CustomRejectedExecutionHandler implements RejectedExecutionHandler {
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            try {
+                // 核心改造点，由blockingqueue的offer改成put阻塞方法
+                executor.getQueue().put(r);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void execute(Runnable runnable) {
+        this.pool.execute(runnable);
+    }
+
 }
 
