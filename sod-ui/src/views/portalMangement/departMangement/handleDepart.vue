42ebe92d57e3bfa2c4c224c5e866d7e5cff0a3ad
wangyaya0708 <1078590298@qq.com>
2020-11-30 11:20:07 +0800

    功能完善

--- sod-ui/src/views/portalMangement/departMangement/handleDepart.vue
+++ sod-ui/src/views/portalMangement/departMangement/handleDepart.vue
@@ -1,40 +1,42 @@
 <template>
   <section class="dataDialog">
     <el-form :model="msgFormDialog" :rules="baseFormRules" ref="fromRef" label-width="120px">
-      <el-form-item prop="ddataId" label="部门类型:">
-        <el-select v-model="msgFormDialog.isshow" :disabled="handleDis">
-          <el-option label="国家级" value="01"></el-option>
-          <el-option label="非国家级" value="02"></el-option>
+      <el-form-item prop="depttype" label="部门类型:">
+        <el-select v-model="msgFormDialog.depttype">
+          <el-option label="机构" value="01"></el-option>
+          <el-option label="部门" value="02"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item prop="dataName" label="部门编号:">
-        <el-input clearable size="small" v-model="msgFormDialog.dataName" :disabled="handleDis" />
+      <el-form-item prop="deptcode" label="部门编号:">
+        <el-input clearable size="small" v-model="msgFormDialog.deptcode" :disabled="handleDis" />
       </el-form-item>
-      <el-form-item prop="dataName" label="部门名称:">
-        <el-input clearable size="small" v-model="msgFormDialog.dataName" :disabled="handleDis" />
+      <el-form-item prop="deptname" label="部门名称:">
+        <el-input clearable size="small" v-model="msgFormDialog.deptname" />
       </el-form-item>
-      <el-form-item prop="dataName" label="上级部门:">
-        <el-input clearable size="small" v-model="msgFormDialog.dataName" :disabled="true" />
+      <el-form-item prop="parentCode" label="上级部门:" v-if="msgFormDialog.parentCode !== '0'">
+        <treeselect
+          v-model.trim="msgFormDialog.parentCode"
+          :options="deptOptions"
+          placeholder="请选择上级部门"
+        />
       </el-form-item>
       <el-form-item prop="serialNumber" label="部门排序:">
         <el-input-number
           clearable
           size="small"
           v-model="msgFormDialog.serialNumber"
-          :disabled="handleDis"
         />
       </el-form-item>
-      <el-form-item prop="serialNumber" label="部门级别:">
+      <el-form-item prop="deptLevel" label="部门级别:">
         <el-input-number
           clearable
           size="small"
-          v-model="msgFormDialog.serialNumber"
-          :disabled="handleDis"
+          v-model="msgFormDialog.deptLevel"
         />
       </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
-      <el-button type="primary" @click="trueDialog('fromRef')" v-if="handleDis">审 核</el-button>
+      <el-button type="primary" @click="trueDialog('fromRef')">确 定</el-button>
       <el-button @click="cancelDialog()">取 消</el-button>
     </div>
   </section>
@@ -44,11 +46,14 @@
 import {
   getById,
   editById,
-  typicalAppSave,
-} from "@/api/portalMangement/typicalManagement";
+  saveDepartManage,
+  treeselect,
+} from "@/api/portalMangement/departMangement";
+import Treeselect from "@riophae/vue-treeselect";
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
 export default {
   name: "dataDialog",
-  components: {},
+  components: { Treeselect },
   props: {
     handleObj: {
       type: Object,
@@ -58,30 +63,37 @@ export default {
     return {
       imageUrl: "",
       handleDis: false,
+      deptOptions: [],
       //编辑页面列
       msgFormDialog: {
-        ddataId: "",
-        dataName: "",
-        icon: "",
-        isshow: "Y",
+        depttype: "",
+        deptcode:"",
+        deptname: "",
+        parentCode: "",
         serialNumber: "1",
-        module: "1",
+        deptLevel: "",
       },
       baseFormRules: {
-        ddataId: [
-          { required: true, message: "请输入四级编码", trigger: "blur" },
+        depttype: [
+          { required: true, message: "请选择部门类型", trigger: "change" },
         ],
-        dataName: [
-          { required: true, message: "请输入分类名称 ", trigger: "blur" },
+        deptcode: [
+          { required: true, message: "请输入部门编号 ", trigger: "blur" },
         ],
-
-        serialNumber: [
-          { required: true, message: "请输入排序编号 ", trigger: "blur" },
+        deptname: [
+          { required: true, message: "请输入部门名称 ", trigger: "blur" },
+        ],
+        parentCode: [
+          { required: true, message: "请选择上级部门", trigger: "change" },
+        ],
+        deptLevel: [
+          { required: true, message: "请选择部门级别", trigger: "change" },
         ],
       },
     };
   },
   async created() {
+    this.getTreeselect();
     // 详情回显
     if (this.handleObj.id) {
       this.handleDis = true;
@@ -123,7 +135,7 @@ export default {
               this.$emit("cancelDialog");
             });
           } else {
-            typicalAppSave(this.msgFormDialog).then((res) => {
+            saveDepartManage(this.msgFormDialog).then((res) => {
               this.$message({
                 type: "success",
                 message: "增加成功",
@@ -138,6 +150,13 @@ export default {
     //取消按钮
     cancelDialog() {
       this.$emit("cancelDialog");
+    },
+
+    /** 查询部门下拉树结构 */
+    getTreeselect() {
+      treeselect().then((response) => {
+        this.deptOptions = response.data;
+      });
     },
   },
 };
