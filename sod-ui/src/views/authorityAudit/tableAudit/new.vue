5ff64032ea12f6913be95b6740c567ababbfaea9
yululuya <whaleyl@163.com>
2021-03-20 00:05:47 +0800

    no message

---
+++ sod-ui/src/views/authorityAudit/tableAudit/new.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="auditHistoryTem"></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.auditHistoryTem {
+}
+</style>
