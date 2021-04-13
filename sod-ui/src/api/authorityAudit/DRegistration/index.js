70d6cfc44c4a69121ea03e28a652aa27b9bda3c8
cuiwenhui <810073399@qq.com>
2021-04-13 19:08:06 +0800

    优化

--- sod-ui/src/api/authorityAudit/DRegistration/index.js
+++ sod-ui/src/api/authorityAudit/DRegistration/index.js
@@ -1,27 +1,10 @@
 import request from "@/utils/request";
 const baseUrl = process.env.VUE_APP_DM;
-// 获取数据分类
-export function getDataClassify() {
-  return request({
-    url: baseUrl + "/dm/newdataApply/getDataTypeList",
-    method: "get"
-  });
-}
 
 //查询注册数据
 export function getDataTable(query) {
   return request({
-    url: baseUrl + "/dm/newdataApply/list",
-    method: "get",
-    params: query
-  });
-}
-
-
-// 根据专题ID查数据库
-export function databaseGet(query) {
-  return request({
-    url: baseUrl + "/dm/database/get",
+    url: baseUrl + "/dm/dataclassapply/list",
     method: "get",
     params: query
   });
@@ -32,5 +15,14 @@ export function deleteById(query) {
     url: baseUrl + "/dm/newdataApply/deleteById",
     method: "delete",
     params: query
+  });
+}
+
+// 删除
+export function review(data) {
+  return request({
+    url: baseUrl + "/dm/dataclassapply/review",
+    method: "post",
+    data: data
   });
 }
